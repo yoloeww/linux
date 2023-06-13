b84f4ab42325673efbed7935a90cb333f941fb02
yolo <77111763+yoloeww@users.noreply.github.com>
2023-06-13 22:01:45 +0800

    Create 14.mysignal.cpp

---
+++ 14.mysignal.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <unistd.h>
+#include <signal.h>
+using namespace std;
+static vector<int> sigarr = {2};
+
+static void show_pending(const sigset_t &pending) {
+    
+}
+int main() {
+    return 0;
+}
